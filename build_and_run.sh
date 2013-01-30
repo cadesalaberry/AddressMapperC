67b2e3120d141477237ca69184de93c0748f2a7f
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-01-30 14:52:43 -0500

    Mod: Store results in same folder. Reduced nb of variables to create to 100

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,22 +1,24 @@
 #1/bin/bash
 
 PROG_NAME=addressmapper
-
+FOLDER=results
 echo
-echo Getting physical address map to file ../result/phys_addr_map
-echo $(cat /proc/iomem) > ../result/phys_addr_map
-cd ./bin
+echo Creating results folder "$FOLDER".
+mkdir $FOLDER
+echo
+echo Getting physical address map to file "./$FOLDER/phys_addr_map".
+echo $(cat /proc/iomem) > ./$FOLDER/phys_addr_map
 echo 
 echo Building...
-gcc -Wall -o $PROG_NAME ../$PROG_NAME.c
+gcc -Wall -o $PROG_NAME ./$PROG_NAME.c
 echo
 echo Running with randomization of the virtual address  space disabled.
 echo
 for MODE in {0..4..1}
 do
 	echo Mode $MODE: Computing...	
-	MIN_AND_MAX=$(setarch x86_64 --addr-no-randomize ./$PROG_NAME $MODE 100000000000)
-	echo $MIN_AND_MAX >> mode$MODE.csv
+	MIN_AND_MAX=$(setarch i686 --addr-no-randomize ./$PROG_NAME $MODE 1000)
+	echo $MIN_AND_MAX >> ./$FOLDER/mode$MODE.csv
 	echo Boundaries writen to files.
 done
 echo
