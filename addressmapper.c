cea83af12ade69d8481ba6fb17a973202b2a612c
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-02-20 23:43:05 -0500

    Fix: Now works for all types.

--- addressmapper.c
+++ addressmapper.c
@@ -1,89 +1,54 @@
 #include <stdio.h>			// Needed for I/O to stdout.
 #include <stdlib.h>			// Standard Library.
 #include <unistd.h>			// Needed to get PID of a process.
+#include <string.h>
+#include "addressmapper.h"
 
-#define STACK_ADDR 0
-#define HEAP_ADDR 1
-#define DATA_ADDR 2
-#define MEMORY_ADDR 3
-#define TEXT_ADDR 4
 
-void * get_addr_according_to_mode(int mode, int size){
-	
-	switch(mode){
-		case STACK_ADDR: {
-			return alloca(size * sizeof (char));
-			break;
-		}
-		case HEAP_ADDR: {
-			return malloc(size * sizeof (char));
-			break;
-		}
-		case DATA_ADDR: {
-			char not_init[size];
-			return not_init;
-			break;
-		}
-		case MEMORY_ADDR: {
-			printf("\n");
-			break;
-		}
-		case TEXT_ADDR: {
-			printf("\n");
-			break;
-		}
-		default:{
-			printf("%i is not a valid argument.\n", mode);
-			exit(-1);
-			break;
-		}
-	}
-	return 0;
-}
+int main(int argc, char *argv[]) {
 
-int display_pid_map(){
-	char sInput[69];
-	// Displays the map for the running process
-	sprintf(sInput, "cat /proc/%d/maps", getpid());
-	printf("%s", sInput);
-	
-	if (fgets(sInput, sizeof sInput, stdin) != NULL){
-	
-	}
-	
-	return 0;
-}
-int main(int argc, char *argv[]){
-	
-	int i;
-	void * min;
-	void * temp;
-	void * max;
-	int mode = atoi(argv[1]);
-	int nb_try = atoi(argv[2]);
-	
-	if (argc != 3){
-		printf("Wrong usage. Available commands:\n");
-		printf("0\t Will estimate stack segment size.\n");
-		printf("1\t Will estimate heap segment size.\n");
-		printf("2\t Will estimate BSS and data segment size.\n");
-		printf("3\t Will estimate memory segment size.\n");
-		printf("4\t Will estimate text segment size.\n");
-	}
-	
-	temp = get_addr_according_to_mode(mode, 1);
-	min = temp;
-	max = temp;
-	printf("nb_try: %i\n", nb_try);
-	for (i = 0; i < nb_try; i++){
+	if(argc == 2) {
+		if(!strcmp(argv[1], "-stack") || !strcmp(argv[1], "-s")) {
+
+			// Gets stack segment bounds
+			printf("Stack:\t%p - ", stack_sgmt2(START));
+			printf("%p\n", stack_sgmt2(END));
+
+		} else if(!strcmp(argv[1], "-memory") || !strcmp(argv[1], "-m")) {
 		
-		temp = get_addr_according_to_mode(mode, nb_try);
-		
-		// Finds out if it is out of the boundaries.
-		// If it is make it a boundary.
-		if (temp < min) min = temp;
-		else if (temp > max) max = temp;
+			// Gets memory segment bounds
+			printf("Memory:\t%p - ", memory_sgmt(START));
+			printf("%p\n", memory_sgmt(END));
+
+		} else if(!strcmp(argv[1], "-text") || !strcmp(argv[1], "-t")) {
+
+			// Gets text segment bounds
+			printf("Text:\t%p - ", text_sgmt(START));
+			printf("%p\n", text_sgmt(END));
+
+		} else if(!strcmp(argv[1], "-data") || !strcmp(argv[1], "-d")) {
+
+			// Gets data segment bounds
+			printf("Data:\t%p - ", data_sgmt(START));
+			printf("%p\n", data_sgmt(END));
+
+		} else if(!strcmp(argv[1], "-bss") || !strcmp(argv[1], "-b")) {
+
+			// Gets memory info
+			printf("BSS:\t%p - ", bss_sgmt(START));
+			printf("%p\n", bss_sgmt(END));
+
+		} else if(!strcmp(argv[1], "-heap") || !strcmp(argv[1], "-h")) {
+
+			// Gets memory info
+			printf("Heap:\t%p - ", heap_sgmt(START));
+			printf("%p\n", heap_sgmt(END));
+
+		}
+
+	} else {
+		printf("Wrong usage.\n");
 	}
-	printf("%p, %p\n", min, max);
+
 	return 0;
 }
